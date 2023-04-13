d988ed2407a81f8ad522e99ab99fc357fe667c7c
denzubechi <chukwumasamuel371@gmail.com>
2023-04-12 23:50:55 -0700

    feat:fixed cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import colors from 'colors'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-
+import cors from 'cors';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -14,7 +14,6 @@ import uploadRoutes from './routes/uploadRoutes.js'
 dotenv.config()
 
 connectDB()
-var cors = require('cors')
 const app = express()
 
 if (process.env.NODE_ENV === 'development') {
